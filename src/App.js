a092ab6ea357ef63c4d5a67ab9651a4bc43cbe77
Omer Ram <0meram95@gmail.com>
2022-06-02 14:47:59 +0300

    ready for use

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import React from "react";
 import Fetcher from "./components/Fetcher.jsx";
 import Poster from "./components/Poster.jsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <div className="App">
         <h1>my first react query</h1>
+        <div>
+        <h2>Benefits of using React-Query in react.js</h2>
+        applications Some of the features that React-Query provides include:
+        <ul style={{ listStyleType: 'none'}}>
+          <li>
+            Using window focus pre-fetching mechanism to pre-fetch the data
+            depending on application tab activity.
+          </li>
+          <li>
+            We can set the number of request retries for any request, in case of
+            random errors.
+          </li>
+          <li>
+            React-Query performs pre-fetching so that the application can update
+            stale data in the background.
+          </li>
+          <li>
+            Handling complex application caching so that the request operation
+            is optimized.
+          </li>
+        </ul>
+      </div>
         <div style={{ display: "flex", justifyContent: "space-around" }}>
           <Fetcher />
           <Poster />
--- src/components/Fetcher.jsx
+++ src/components/Fetcher.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 async function fetchPosts() {
   const { data } = await axios.get(
@@ -21,32 +20,11 @@ const Fetcher = () => {
   }
 
   return (
-    <div style={{ textAlign: "left", width: "600px", marginLeft: "100px" }}>
-      <ReactQueryDevtools />
-      <div>
-        <h2>Benefits of using React-Query in react.js</h2>
-        applications Some of the features that React-Query provides include:
-        <ul>
-          <li>
-            Using window focus pre-fetching mechanism to pre-fetch the data
-            depending on application tab activity.
-          </li>
-          <li>
-            We can set the number of request retries for any request, in case of
-            random errors.
-          </li>
-          <li>
-            React-Query performs pre-fetching so that the application can update
-            stale data in the background.
-          </li>
-          <li>
-            {" "}
-            Handling complex application caching so that the request operation
-            is optimized.
-          </li>
-        </ul>
-      </div>
-      <h1>Posts using <span style={{color: 'red'}}>useQuery('posts', fetchPosts)</span></h1>
+    <div style={{ textAlign: "left", width: "700px", marginLeft: "100px" }}>
+      <h1>
+        Posts using{" "}
+        <span style={{ color: "red" }}>useQuery('posts', fetchPosts)</span>
+      </h1>
       {data.map((post, index) => {
         return (
           <div style={{ margin: "8px" }} key={index}>
